7efc8bc230a7255b68c6e166612e8a66f0523d28
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-05-29 18:08:16 -0400

    Add methods for each brainfuck operator

--- python/bf2.py
+++ python/bf2.py
@@ -1,3 +1,4 @@
+import sys
 import enum
 
 
@@ -14,13 +15,37 @@ class Token(enum.Enum):
 
 class Interpreter:
 	def __init__(self, program: str, c_size: int = 8, m_size: int = 30000):
-		self.stack = []             # stack for keeping track of loops
-		self.tokens = []            # tokenized list of brainfuck instructions
-		self.data_ptr = 0           # pointer to current cell in memory
-		self.instr_ptr = 0          # pointer to current instruction
-		self.program = program      # string of brainfuck script
-		self.cell_size = c_size     # size of memory cells (in bits)
-		self.memory = [0] * m_size  # array of cells that serves as program memory
+		self.stack = []                   # stack for keeping track of loops
+		self.tokens = []                  # tokenized list of brainfuck instructions
+		self.data_ptr = 0                 # pointer to current cell in memory
+		self.instr_ptr = 0                # pointer to current instruction
+		self.program = program            # string of brainfuck script
+		self.cell_size = c_size           # size of memory cells (in bits)
+		self.memory = [0] * m_size        # array of cells that serves as program memory
+		self.max_int = 2 << (c_size - 1)  # max int size (greater values will wrap around past 0)
+
+	def incr_ptr(self):
+		self.data_ptr += 1
+		if self.data_ptr == len(self.memory):
+			self.memory += [0] * (len(self.memory) // 2)
+
+	def decr_ptr(self):
+		self.data_ptr -= 1
+
+	def incr(self):
+		self.memory[self.data_ptr] += 1
+		self.memory[self.data_ptr] %= (2 << (self.cell_size - 1))
+
+	def decr(self):
+		self.memory[self.data_ptr] -= 1
+		self.memory[self.data_ptr] %= (2 << (self.cell_size - 1))
+
+	def stdout(self):
+		sys.stdout.write(self.memory[self.data_ptr])
+		sys.stdout.flush()
+
+	def stdin(self):
+		self.memory[self.data_ptr] = ord(sys.stdin.read(1))
 
 	def parse(self):
 		while self.instr_ptr < len(self.program):
